31ba88db9ac85abbfb1b6cb47897e3c66f19cf2d
SukbeomH <brent93.dev@gmail.com>
2022-07-22 17:31:54 +0900

    api controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { ApiControllerController } from './api-controller/api-controller.controller';
 
 @Module({
-  imports: [],
-  controllers: [UsersController],
+  imports: [UsersModule],
+  controllers: [UsersController, ApiControllerController],
   providers: [],
 })
 export class AppModule {}
